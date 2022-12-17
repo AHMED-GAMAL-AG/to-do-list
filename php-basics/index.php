4c459d11a6cc91527ab8755179efafd578a8301c
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-12-17 11:03:35 +0200

    02. المتغيرات Variables

---
+++ php-basics/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$name ='ahmed';
+
+echo 'hello  $name'; echo '<br>';
+echo "hello  {$name}"; echo '<br>'; // use " to use varables inside {} is just to make it readable"
+echo 'hello ' . $name; echo '<br>';
