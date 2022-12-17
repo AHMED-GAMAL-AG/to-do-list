9ac0266e2383ef38fa7513f39b22355486c801a9
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-12-17 13:46:35 +0200

    03. عوامل المقارنة

--- php-basics/index.php
+++ php-basics/index.php
@@ -1,5 +1,19 @@
 <?php
 
-// common approuch is to use UPPER CASE LETTERS when declaring a const
-const MAX_NUMMBER = 100;
-echo MAX_NUMMBER;
+$a= 1;
+$b= '1';
+
+echo 'a is not identecal to b true :' . $a !== $b; echo '<hr>';
+
+// space ship operator 0 if eqaul ### 1 if left is greater ### -1 if right it greater
+$a= 1;
+$b= 1;
+echo $a <=> $b; echo '<hr>';
+
+$a= 2;
+$b= 1;
+echo  $a <=> $b; echo '<hr>';
+
+$a= 1;
+$b= 2;
+echo  $a <=> $b; echo '<hr>';
