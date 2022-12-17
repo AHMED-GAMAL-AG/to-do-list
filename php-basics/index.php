2983e1b556b995d7895fb4dc5148816c6bb94583
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-12-17 16:19:25 +0200

    02. الحلقة for

--- php-basics/index.php
+++ php-basics/index.php
@@ -1,10 +1,3 @@
 <?php
 
-// null coalescing operator (PHP v.7)
-
-$name = 'ahmed';
-$email = 'email@example.com';
-
-echo isset($name) ? $name : 'nobody'; echo '<hr>';
-
-echo $name ?? $email ?? 'nobody'; // email if not defined then email if not defined then nobody
+for ($i = 0, $j = 1; $i <= 10; print $i , $i++);
