17f89e28dd5add6e494e29736910713ba086dd7b
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-12-17 14:17:44 +0200

    04. التحقق من القيم باستخدام isset

--- php-basics/index.php
+++ php-basics/index.php
@@ -1,19 +1,11 @@
 <?php
 
-$a= 1;
-$b= '1';
 
-echo 'a is not identecal to b true :' . $a !== $b; echo '<hr>';
+$a = 0;
+echo isset($a) ? 'defined' :'undefined' ; echo '<hr>';
 
-// space ship operator 0 if eqaul ### 1 if left is greater ### -1 if right it greater
-$a= 1;
-$b= 1;
-echo $a <=> $b; echo '<hr>';
+const b = 100;
+// echo isset(b) ? 'defined' :'undefined' ; // parse error use defined() instead
 
-$a= 2;
-$b= 1;
-echo  $a <=> $b; echo '<hr>';
-
-$a= 1;
-$b= 2;
-echo  $a <=> $b; echo '<hr>';
+echo defined('b') ? 'defined' :'undefined' ; echo '<hr>';
+echo defined("b") ? 'defined' :'undefined' ; echo '<hr>';
