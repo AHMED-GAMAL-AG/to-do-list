fe2828d40590cf1962822db5a945e65b471be75d
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-12-18 10:15:35 +0200

    03. عبور المصفوفات باستخدام الحلقات

--- php-basics/index.php
+++ php-basics/index.php
@@ -1,12 +1,38 @@
 <?php
 
-$names = [
-    'name' => 'ahmed',
-    'age' => 25,
-    'job' => 'software engineer',
-    'skills' => ['PHP', 'MySQL', 'CSS', 'HTML5'],
+$employes = [
+    [
+        'name' => 'ahmed',
+        'age' => 25,
+        'job' => 'software engineer',
+        'skills' => ['PHP', 'MySQL', 'CSS', 'HTML5', 'problem solving'],
+    ],
+
+    [
+        'name' => 'mohamed',
+        'age' => 30,
+        'job' => 'software engineer',
+        'skills' => ['python', 'django', 'CSS', 'HTML5'],
+    ],
 ];
 
-echo $names['skills'][1]; echo '<hr>';
-$names['skills'][]= 'OOP';
-echo $names['skills'][4];
+$count = count($employes);
+for ($i = 0; $i < $count; $i++) {
+    echo $employes[$i]['name'];
+    for ($j = 0; $j < count($employes[$i]['skills']); $j++) {
+        echo ' - ' . $employes[$i]['skills'][$j];
+    }
+    echo '<hr>';
+}
+
+$i = 0;
+while ($i < $count) {
+    $j = 0;
+    echo $employes[$i]['name'];
+    while ($j < count($employes[$i]['skills'])) {
+        echo ' - ' . $employes[$i]['skills'][$j];
+        $j++;
+    }
+    $i++;
+    echo '<hr>';
+}
