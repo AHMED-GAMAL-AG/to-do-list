494b4d0403a9dba778d2ff10ede0c99764fb24e6
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-12-19 08:09:21 +0200

    03. العامل arguments

--- php-basics/index.php
+++ php-basics/index.php
@@ -1,34 +1,16 @@
 <?php
 
-$employes = [
-    [
-        'name' => 'ahmed',
-        'age' => 25,
-        'job' => 'software engineer',
-        'skills' => ['PHP', 'MySQL', 'CSS', 'HTML5', 'problem solving'],
-    ],
-
-    [
-        'name' => 'mohamed',
-        'age' => 30,
-        'job' => 'software engineer',
-        'skills' => ['python', 'django', 'CSS', 'HTML5'],
-    ],
-];
-
-$skill_count = count($employes[1]['skills']);
-
-foreach ($employes as $employee) {
-    echo $employee['name'] . ' - ' . $employee['job'];
-    echo '<hr>';
+function sum(...$numbers) // pass as many agrguiments as you want it will be put in an array $numbers
+{
+    $sum = 0;
+    foreach ($numbers as  $num) {
+        $sum += $num;
+    }
+    echo $sum;
 }
 
+sum (5,5,50);
 echo '<hr>';
 
-foreach ($employes as $key => $employee) {
-    echo $employee['name'];
-    for ($i = 0; $i < count($employes[$key]['skills']); $i++) {
-        echo ' - ' . $employee['skills'][$i];
-    }
-    echo '<hr>';
-}
+$numbers = [1,2,3,4,5];
+sum (...$numbers); // put ... to pass an array
