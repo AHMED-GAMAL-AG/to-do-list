dc52a7d7385ac96cc58db7185870d7482cb59ea2
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-12-20 10:55:35 +0200

    04. التغليف Encapsulation

--- php-basics/index.php
+++ php-basics/index.php
@@ -2,14 +2,36 @@
 
 class Vehicle
 {
-    public $modle;
-    public $year;
+    private $modle;
+    private $year;
 
     public function __construct($m, $y)
     {
         $this->modle = $m;
         $this->year = $y;
     }
+    public function setmodel($m)
+    {
+        $this->modle = $m;
+    }
+
+    public function getmodel()
+    {
+        return $this->modle;
+    }
+
+    public function setyear($y)
+    {
+        if (is_int($y)) {
+            $this->year = $y;
+        } else throw new Exception('please enter a degit');
+    }
+
+    public function getyear()
+    {
+        return $this->year;
+    }
+
 
     public function start()
     {
@@ -17,6 +39,9 @@ class Vehicle
     }
 }
 
-$v1 =  new Vehicle("BMW" , 2023);
-
+$v1 =  new Vehicle("BMW", 's');
+echo $v1->setmodel('toyota');
+echo $v1->getmodel();
+echo $v1->setyear(2023);
+echo '<hr>';
 $v1->start();
