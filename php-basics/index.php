707a7d4df7c911a702ad16bd8ed6bad6a50e8f7b
jimmy-glitch <76778937+AHMED-GAMAL-AG@users.noreply.github.com>
2022-12-23 15:55:44 +0200

    01. PHP في عالم الويب

--- php-basics/index.php
+++ php-basics/index.php
@@ -1,25 +1,21 @@
 <?php
-
-
-require 'database/_init.php';
+require '_init.php';
 
 $tasks = QueryBuilder::get('tasks');
-QueryBuilder::update('tasks', 3, [
-    'description' => 'updated task from index',
-    'completed' => 1,
-]);
 
-QueryBuilder::delete('tasks' , 0 ,'id' , '>'); // delete from tasks where id > 0
-
-// QueryBuilder::insert('tasks', [
-//     "description" => 'task from index after passing data ',
+// QueryBuilder::update('tasks', 3, [
+//     'description' => 'updated task from index',
 //     'completed' => 1,
 // ]);
 
-foreach ($tasks as $task) {
-    echo "{$task->description} <hr>";
-}
+// QueryBuilder::delete('tasks' , 0 ,'id' , '>'); // delete from tasks where id > 0
 
-echo '<pre>';
-var_dump($tasks);
-echo '</pre>';
+// QueryBuilder::insert('tasks', [
+//     "description" => 'third task',
+//     'completed' => 1,
+// ]);
+
+    require 'index.view.php';
+?>
+
+
